c8360b6f8c21f94e5066b206e65c2db630d75b6b
Ronald Baumgarten <baumgartendj@gmail.com>
2025-05-22 16:18:40 +0200

    Add Produkt-class

---
+++ Produkt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProdukt
+{
+    public class Produkt
+    {
+        private string name;
+        private double preis;
+        private string hersteller;
+
+        public String Name { get => name; set => name = value; }
+        public double Preis { get => preis; set => preis = value; }
+        public string Hersteller { get => hersteller; set => hersteller = value; }
+        public Produkt(string name, double preis, string hersteller)
+        {
+            this.name = name;
+            this.preis = preis;
+            this.hersteller = hersteller;
+        }
+
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,60 @@
-﻿namespace ConsoleProdukt
+﻿using System.ComponentModel;
+
+namespace ConsoleProdukt
 {
     internal class Program
     {
+        private Produkt[] produkte = new Produkt[100];
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int auswahl = 1;
+            while (auswahl >= 1)
+            {
+                Console.WriteLine("Bitte waehlen sie aus:");
+                Console.WriteLine("1. Produkt hinzufuegen");
+                Console.WriteLine("2. Produkte auslisten");
+                Console.WriteLine("3. Produkte nach Preis sortieren");
+                Console.WriteLine("0. Programm beenden");
+
+                if (Int32.TryParse(Console.ReadLine(), out auswahl) && auswahl >= 0 && auswahl < 4)
+                {
+                    switch (auswahl)
+                    {
+                        case 1:
+                            addProduct();
+                            break;
+                        case 2:
+                            listProducts();
+                            break;
+                        case 3:
+                            sortProducts();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Bitte treffe sie eine gueltige Auswahl!");
+                }
+            }
+            Console.WriteLine("Bye!");
         }
+
+        public static void addProduct()
+        {
+
+        }
+        public static void listProducts()
+        {
+
+        }
+
+        public static void sortProducts()
+        {
+
+        }
+
     }
 }
